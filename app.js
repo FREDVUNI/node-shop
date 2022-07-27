f2c43efb51fbf0825deb295367b439229780b0c3
TAATA EARL <fredvuni809@gmail.com>
2022-07-27 15:49:20 +0300

    Added a default heroku route

--- app.js
+++ app.js
@@ -10,17 +10,16 @@ app.use(express.urlencoded({extended:true}))
 
 app.use(morgan("tiny"))
 
-// app.use("/",(req,res)=>{
-//     res.status(200)
-//     .json({
-//         message:"API for the e-commerce store. Check out it's documentation below 🐻",
-//         api_documentation: "https://documenter.getpostman.com/view/21884902/UzJQqEYA"
-//     })
-// })
+app.use("/",(req,res)=>{
+    res.status(200)
+    .json({
+        message:"API for the e-commerce store. Check out it's documentation below 🐻",
+        api_documentation: "https://documenter.getpostman.com/view/21884902/UzJQqEYA"
+    })
+})
 
 app.use("/api/v1/products",require("./routes/ProductRoutes"))
 app.use("/api/v1/categories",require("./routes/CategoryRoutes"))
 app.use("/api/v1/users",require("./routes/UserRoutes"))
-// app.use("/api/v1/cart",require("./routes/CartRoutes"))
 
 module.exports = app
