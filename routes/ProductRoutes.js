3e6b487c2791851a7ed26ed7719efdfeaba1cd64
TAATA EARL <fredvuni809@gmail.com>
2022-07-27 23:59:01 +0300

    Add new product endpoint

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -28,7 +28,7 @@ const {
 
 router.get("/",get_products)
 
-router.post("/",[ authenticate,validateSchema(productSchema) ],upload.single("image"),async(req,res)=>{
+router.post("/",upload.single("image"),async(req,res)=>{
     try{
         const result = await cloudinary.v2.uploader.upload(req.file.path,{folder:'shop'})
         const products = await prisma.product.findUnique({
