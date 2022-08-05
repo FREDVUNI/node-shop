be3be295d51967c02fcff4088a3fe9823d72a8e7
TAATA EARL <fredvuni809@gmail.com>
2022-08-05 13:43:07 +0300

    Changed responses of all categories fetched

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -7,7 +7,7 @@ const get_categories = async(req,res) =>{
         const categories = await prisma.category.findMany({
             include:{products:true},
         })
-        res.status(200).json({message:"all categories",data:categories})
+        res.status(200).json(categories)
     }
     catch(error){
         res.status(500).json(error.message || "There was a server error.")
